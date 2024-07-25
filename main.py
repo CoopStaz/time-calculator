6271769949f58ab2200ab5728832b74ccd0423f8
CoopStaz <cjstaz13@gmail.com>
2024-07-25 22:20:57 +1000

    Fix bugs

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ PM_OR_AM = "AM"
 def decimal_to_time(decimal):
     hours = int(decimal)
     minutes = round((decimal - hours) * 60)
+
+    # Ensure there is a 0 in front of the number if it is single digit
     if minutes < 10:
         minutes = f"0{minutes}"
+
+    # If it is midnight, change the time from 00 to 12
+    if hours == 0:
+        hours = 12
+
     return hours, minutes
 
 
@@ -23,24 +30,24 @@ def add_time(start, duration, starting_day=None):
     start_minute = int(start_time_array[1]) / 60
 
     start_time_decimal = start_hour + start_minute
-    print(start_time_decimal)
 
     # Calculates the duration times
     duration_array = duration.split(":")
 
     duration_hour = int(duration_array[0])
     duration_minute = int(duration_array[1]) / 60
-    print(duration_minute)
 
     duration_time_decimal = duration_hour + duration_minute
 
     # Gets the sum of both the start time and the duration time to get the new time in decimal form
     total_time_decimal = start_time_decimal + duration_time_decimal
+    print(total_time_decimal)
 
     # If the total time is greater than 24 it adds to the amount of days ahead
     days_added = 0
     if total_time_decimal > 24:
-        days_added += total_time_decimal // 24
+        days_added += int(total_time_decimal // 24)
+        print(days_added)
         total_time_decimal -= (24 * days_added)
 
     # If the time is greater than 12 the time is in PM
@@ -53,23 +60,30 @@ def add_time(start, duration, starting_day=None):
     # Converts the time decimal into digital time
     time = decimal_to_time(total_time_decimal)
 
-    if not starting_day:
+    # If the time is 12 then it should change the time to PM
+    if time[0] == 12:
+        PM_OR_AM = "PM"
+
+    if days_added == 1:
+        return f"{time[0]}:{time[1]} {PM_OR_AM} (next day)"
+    elif not starting_day:
         if days_added < 1:
             return f"{time[0]}:{time[1]} {PM_OR_AM}"
+        elif days_added > 1:
+            return f"{time[0]}:{time[1]} {PM_OR_AM} ({int(days_added)} days later)"
         else:
-            if days_added > 1:
-                return f"{time[0]}:{time[1]} {PM_OR_AM} ({int(days_added)} days later)"
-            else:
-                return f"{time[0]}:{time[1]} {PM_OR_AM} ({int(days_added)} day later)"
+            return f"{time[0]}:{time[1]} {PM_OR_AM} ({int(days_added)} day later)"
     else:
         formatted_day = starting_day.capitalize()
+        print(formatted_day)
         day_index = DAYS_OF_THE_WEEK.index(formatted_day)
         new_index = day_index + days_added
 
         if new_index > DAYS_OF_THE_WEEK.index(DAYS_OF_THE_WEEK[-1]):
             index_in_range = int(new_index // 7)
             return f"{time[0]}:{time[1]} {PM_OR_AM}, {DAYS_OF_THE_WEEK[index_in_range]} ({int(days_added)} days later)"
+        else:
+            return f"{time[0]}:{time[1]} {PM_OR_AM}, {formatted_day}"
 
 
-print(add_time('3:30 PM', '2:12'))
-print(add_time('11:55 AM', '3:12'))
+print(add_time('11:59 PM', '24:05'))
